b222636ef080eb8621a94834ca5143685fee91b1
Montes <chris@montesmakes.co>
2026-04-15 15:10:21 -0500

    fix: Rails should be 7.2 not 8.0

--- db/migrate/20260414193419_create_cached_forecasts.rb
+++ db/migrate/20260414193419_create_cached_forecasts.rb
@@ -1,4 +1,4 @@
-class CreateCachedForecasts < ActiveRecord::Migration[8.0]
+class CreateCachedForecasts < ActiveRecord::Migration[7.2]
   def change
     create_table :cached_forecasts do |t|
       t.jsonb :forecast_data, null: false
